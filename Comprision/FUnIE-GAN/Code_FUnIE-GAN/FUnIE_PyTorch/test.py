00d30384b1497043fc9421e23bcb2542936645ac
nuochenhere-123 <nuochenhere@163.com>
2025-01-11 12:17:55 +0800

    FUnIE-GAN-1

---
+++ Comprision/FUnIE-GAN/Code_FUnIE-GAN/FUnIE_PyTorch/test.py
@@ -0,0 +1,101 @@
+"""
+ > Script for testing .pth models  
+    * set model_name ('funiegan'/'ugan') and  model path
+    * set data_dir (input) and sample_dir (output) 
+"""
+# py libs
+import os
+import time
+import argparse
+import numpy as np
+from PIL import Image
+from glob import glob
+from ntpath import basename
+from os.path import join, exists
+# pytorch libs
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision.utils import save_image
+import torchvision.transforms as transforms
+
+## options
+parser = argparse.ArgumentParser()
+parser.add_argument("--data_dir", type=str, default="data/test/Test-UIQS/")
+parser.add_argument("--sample_dir", type=str, default="data/output256/FUnIE-UIQS")
+# parser.add_argument("--sample_dir", type=str, default="data/output-ToResize/FUnIE-110")
+parser.add_argument("--model_name", type=str, default="funiegan") # or "ugan"
+parser.add_argument("--model_path", type=str, default="checkpoints_reTrain/FunieGAN/UIEB780/generator_200.pth")
+opt = parser.parse_args()
+
+## checks
+assert exists(opt.model_path), "model not found"
+os.makedirs(opt.sample_dir, exist_ok=True)
+is_cuda = torch.cuda.is_available()
+Tensor = torch.cuda.FloatTensor if is_cuda else torch.FloatTensor 
+
+## model arch
+if opt.model_name.lower()=='funiegan':
+    from nets import funiegan
+    model = funiegan.GeneratorFunieGAN()
+elif opt.model_name.lower()=='ugan':
+    from nets.ugan import UGAN_Nets
+    model = UGAN_Nets(base_model='pix2pix').netG
+else: 
+    # other models
+    pass
+
+## load weights
+model.load_state_dict(torch.load(opt.model_path))
+if is_cuda: model.cuda()
+model.eval()
+print ("Loaded model from %s" % (opt.model_path))
+
+# # 256*256 尺寸输入输出
+# # data pipeline
+# img_width, img_height, channels = 256, 256, 3
+# transforms_ = [transforms.Resize((img_height, img_width), Image.BICUBIC),
+#                transforms.ToTensor(),
+#                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),]
+# transform = transforms.Compose(transforms_)
+# # 256*256 尺寸输入输出
+
+## testing loop
+i = 0
+times = []
+test_files = sorted(glob(join(opt.data_dir, "*.*")))
+for path in test_files:
+    inp_img = Image.open(path).convert("RGB")
+
+    # 近似尺寸 输入输出
+    img_width, img_height = inp_img.size
+    transforms_ = [transforms.CenterCrop((img_height - img_height % 32, img_width - img_width % 32)),  # 确保图像尺寸是32的倍数
+          transforms.ToTensor(),
+          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),]
+    transform = transforms.Compose(transforms_)
+    # 近似尺寸 输入输出
+
+    inp_img = transform(inp_img)
+    inp_img = Variable(inp_img).type(Tensor).unsqueeze(0)
+    # generate enhanced image
+    s = time.time()
+    gen_img = model(inp_img)
+    times.append(time.time()-s)
+    # save output
+    # img_sample = torch.cat((inp_img.data, gen_img.data), -1)
+    img_sample = gen_img.data
+    save_image(img_sample, join(opt.sample_dir, basename(path)), normalize=True)
+    i = i + 1
+    print (i, " Tested: %s" % path)
+
+## run-time    
+if (len(times) > 1):
+    print ("\nTotal samples: %d" % len(test_files)) 
+    # accumulate frame processing times (without bootstrap)
+    Ttime, Mtime = np.sum(times[1:]), np.mean(times[1:]) 
+    print ("Time taken: %d sec at %0.3f fps" %(Ttime, 1./Mtime))
+    print("Saved generated images in in %s\n" %(opt.sample_dir))
+
+
+
